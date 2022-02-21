dfc3755589100809f034ccae94db75e8ef49c41f
manoj <manojreddy.a2@gmail.com>
2022-02-21 19:43:35 +0530

    commit operations

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const title = document.querySelector('#title');
-const author = document.querySelector('#author');
-const pages = document.querySelector('#pages');
-const readStatus = document.querySelector('#status');
+const title = document.querySelector('#title').value;
+const author = document.querySelector('#author').value;
+const pages = document.querySelector('#pages').value;
+const readStatus = document.querySelector('#status').value;
 const submitButton = document.querySelector('.btn-submit');
 
 let myLibrary = [];
@@ -13,7 +13,6 @@ function Book(title, author, pages, readStatus) {
     this.readStatus = readStatus
 }
 
-const myBook = new Book(title.value, author.value, pages.value, readStatus.value)
 
 function addBookToLibrary(myBook) {
     myLibrary.push(myBook)
@@ -21,18 +20,27 @@ function addBookToLibrary(myBook) {
 
 
 
-submitButton.addEventListener('click', function() {
-    addBookToLibrary(myBook)
+submitButton.addEventListener('click', function () {
+    if(title != '' && author != '' && pages != '', readStatus != '') {
+        const myBook = new Book(title, author, pages, readStatus)
+        addBookToLibrary(myBook)
+    } 
     let tr = document.createElement('tr')
-    if(myLibrary){
-        myLibrary.forEach(book => {
-            for (const key in book) {
-                console.log(book[key])
+    let td = document.createElement('td')
 
-                tr.innerHTML += `<td>${book[key]}</td>`
-            }
-            document.querySelector('.table-data').appendChild(tr)
-            console.log(book.title, book.author, book.pages, book.readStatus)
-        })
-    }
+    myLibrary.forEach((book, i) => {
+        myLibrary[i] = Book
+        for (const key in book) {
+            console.log(book[key])
+            tr.innerHTML += `<td>${book[key]}</td>`
+        }
+        document.querySelector('.table-data').appendChild(tr)
+        console.log(book.title, book.author, book.pages, book.readStatus)
+    })
+
+
+    // title = ''
+    // author = ''
+    // pages = ''
+    // readStatus = ''
 })
