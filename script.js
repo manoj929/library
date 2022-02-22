3ad5cc3af5e6687a8af667948cf81af11890fbda
manoj <manojreddy.a2@gmail.com>
2022-02-22 08:09:20 +0530

    edited.script.js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-const title = document.querySelector('#title').value;
-const author = document.querySelector('#author').value;
-const pages = document.querySelector('#pages').value;
-const readStatus = document.querySelector('#status').value;
+// const title = document.querySelector('#title').value;
+// const author = document.querySelector('#author').value;
+// const pages = document.querySelector('#pages').value;
+// const readStatus = document.querySelector('#status').value;
+// const submitButton = document.querySelector('.btn-submit');
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const pages = document.querySelector('#pages');
+const readStatus = document.querySelector('#status');
 const submitButton = document.querySelector('.btn-submit');
 
 let myLibrary = [];
@@ -13,17 +18,14 @@ function Book(title, author, pages, readStatus) {
     this.readStatus = readStatus
 }
 
-function addBookToLibrary(myBook) {
-    myLibrary.push(myBook)
-}
-
-submitButton.addEventListener('click', function () {
-    if(title != '' && author != '' && pages != '', readStatus != '') {
-        const myBook = new Book(title, author, pages, readStatus)
-        addBookToLibrary(myBook)
-    } 
+function addBookToLibrary() {
+    if (title.value != '' && author.value != '' && pages.value != '', readStatus.value != '') {
+        const myBook = new Book(title.value, author.value, pages.value, readStatus.value)
+        myLibrary.push(myBook)
+    } else {
+        alert('please all fields')
+    }
     let tr = document.createElement('tr')
-    let td = document.createElement('td')
 
     myLibrary.forEach((book, i) => {
         myLibrary[i] = Book
@@ -32,6 +34,16 @@ submitButton.addEventListener('click', function () {
             tr.innerHTML += `<td>${book[key]}</td>`
         }
         document.querySelector('.table-data').appendChild(tr)
-        console.log(book.title, book.author, book.pages, book.readStatus)
+        // console.log(book.title, book.author, book.pages, book.readStatus)
     })
+    
+    title.value = '';
+    author.value = '';
+    pages.value = '';
+    readStatus.value = '';
+}
+
+submitButton.addEventListener('click', function (e) {
+    addBookToLibrary()
+    e.preventDefault();
 })
